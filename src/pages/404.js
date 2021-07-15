4d6829c8b335342c8b895bcaa5eb1817f96c3407
Fontenelle <fontenec@ALMAC-FONTENEC.hsd1.co.comcast.net>
2021-07-15 07:23:34 -0600

    added new files

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div>
+            <h2>Sorry, you entered wrong url</h2>
+        </div>
+    )
+}
+
+export default Error
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Coney</div>
 }
